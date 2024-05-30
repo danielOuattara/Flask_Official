cc5b8cb0d9882af4e9e0b79554fd8b36b90ddd75
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-30 23:04:55 +0200

    01 quick_start: 04 routing

---
+++ 01_quick_start/_04_routing.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return " Index page"
+
+
+# @app.route('/hello')
+# def hello():
+#     return 'Hello world of Flask !'
+
+
+@app.route("/<name>")
+def hello_user(name):
+    if name == 'hello':
+        return 'Hello world of Flask !'
+    else:
+        return f"Hello, {escape(name)}!"
+
+"""
+Routing
+--------
+Modern web applications use meaningful URLs to help users. 
+Users are more likely to like a page and come back if the 
+page uses a meaningful URL they can remember and use to 
+directly visit a page.
+
+Use the route() decorator to bind a function to a URL.
+"""
